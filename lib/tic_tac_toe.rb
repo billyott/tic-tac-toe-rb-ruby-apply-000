12d1772d4e0ebcf0fba2f97bc9cfe89ab982e4c0
Billy Ott <willrichott@gmail.com>
2020-07-18 05:43:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,14 +45,15 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
+  move = gets
+  indexed_move = input_to_index(move)
+  until valid_move?(board, indexed_move)
+    puts "Sorry, that's not a valid move. Try again:"
+    move = gets
+    indexed_move = input_to_index(move)
   end
+  board[indexed_move] = "X"
+  display_board(board)
 end
 
 
